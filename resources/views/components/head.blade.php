546c70f324fcee982fad4f11b7c9aa59dbe66b6d
Amir <amir.begic@ext.experienceone.com>
2025-10-25 17:07:15 +0200

    Change favicon

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -10,8 +10,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
 
-<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
-<link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('assets/images/logo.jpeg') }}" type="image/x-icon">
+<link rel="icon" href="{{ asset('assets/images/logo.jpeg') }}" type="image/x-icon">
 
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
